6f0b3773c470f27c389771d65a81972452ba954e
Barroso208 <Barroso208@users.noreply.github.com>
2020-10-03 22:27:14 +0200

    añadimos json

--- js.js
+++ js.js
@@ -2,10 +2,28 @@ function musica(){
     document.getElementById("audio").play();
     document.getElementById("video").play();
 }
-$('*').css('overflow-y','hidden')
-$('input.enviar').on('click',function(e) {
-    if($('#fname').val() == "soylamejor"){
-        $('div.password').css('display','none')
-        $('*').css('overflow-y','')
+
+
+let password;
+const xhr = new XMLHttpRequest();
+xhr.onload = function(){
+    if(xhr.status === 200){
+        responseObject = JSON.parse(xhr.responseText);
+        password = JSON.parse(this.responseText);
+        $('*').css('overflow-y','hidden')
+        $('input.enviar').on('click',function(e) {
+        if($('#fname').val() == password.contraseña){
+            $('div.password').css('display','none')
+            $('*').css('overflow-y','')
     }
 });
+    }
+}
+xhr.open('GET','json.json',true);
+xhr.send();
+
+
+
+
+
+
