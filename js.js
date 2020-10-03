bea231e8dc52b58c3d7c0e36bbf5ab45b0556668
Barroso208 <Barroso208@users.noreply.github.com>
2020-10-03 21:56:34 +0200

    password

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
-
 function musica(){
     document.getElementById("audio").play();
     document.getElementById("video").play();
 }
-
+$('*').css('overflow-y','hidden')
+$('input.enviar').on('click',function(e) {
+    if($('#fname').val() == "soylamejor"){
+        $('div.password').css('display','none')
+        $('*').css('overflow-y','')
+    }
+});
